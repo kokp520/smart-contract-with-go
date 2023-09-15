0c470d84e2678c7d9f16d2b2ac399555f19c5180
adiwu <kokp520@gmail.com>
2023-09-15 21:19:16 +0800

    connect wallet

--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"context"
+	"crypto/ecdsa"
 	"fmt"
 	"log"
+	"math/big"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func main() {
 	//ethereum env adress
-	client, err := ethclient.Dial("http://127.0.0.1:56619")
+	client, err := ethclient.Dial("http://127.0.0.1:7545")
 	// fmt.Println(client)
 
 	if err != nil {
@@ -21,11 +25,52 @@ func main() {
 	}
 
 	// quick
-
 	header, err := client.HeaderByNumber(context.Background(), nil)
 	if err != nil {
 		log.Fatal(err)
 	} else {
-		fmt.Println(header.Number.String())
+		fmt.Printf("here is header Number : %s", header.Number.String())
 	}
+}
+
+func getAccountAuth(client *ethclient.Client, accountAddress string) *bind.TransactOpts {
+
+	privateKey, err := crypto.HexToECDSA(accountAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+
+	if !ok {
+		panic("invalid public key")
+	}
+
+	// if publicKeyECDSA != nil {
+	// 	fmt.Printf(publicKeyECDSA.X.String())
+	// }
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	//fetch the last use nonce of account
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("nounce=", nonce)
+	chainID, err := client.ChainID(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
+	if err != nil {
+		panic(err)
+	}
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)      // in wei
+	auth.GasLimit = uint64(3000000) // in units
+	auth.GasPrice = big.NewInt(1000000)
+
+	return auth
 }
